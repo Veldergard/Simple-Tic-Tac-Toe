0b71d7a4f407a1fa0e17f8ad2c8e15a971ce77a0
vdgd <veldergardw@gmail.com>
2022-07-13 04:51:41 +0300

    v5

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -10,56 +10,53 @@ public class Main {
         char[][] grid = new char[3][3];
         scanner = new Scanner(System.in);
         initGrid(grid);
-        usersGrid(grid);
         printGrid(grid);
-//        printGameState(grid);
-        makePlayerMove(grid);
-        printGrid(grid);
+        while (true) {
+            makePlayerMove(grid, 'X');
+            printGrid(grid);
+            if (checkWin(grid, 'X')) {
+                System.out.println("X wins");
+                break;
+            }
+            makePlayerMove(grid, 'O');
+            printGrid(grid);
+            if (checkWin(grid, 'O')) {
+                System.out.println("O wins");
+                break;
+            }
+        }
     }
 
     public static void initGrid(char[][] grid) {
         for (char[] rows : grid) {
-            Arrays.fill(rows, '_');
+            Arrays.fill(rows, ' ');
         }
     }
 
-    public static void usersGrid(char[][] grid) {
-        System.out.print("Enter cells: ");
-        String cells = scanner.nextLine();
-        if (cells.length() >= 9) {
-            int i = 0;
-            for (int j = 0; j < grid.length; j++) {
-                for (int k = 0; k < grid[j].length; k++) {
-                    grid[j][k] = cells.charAt(i++);
-                }
-            }
-        }
-    }
-
-    public static void makePlayerMove(char[][] grid) {
+    public static void makePlayerMove(char[][] grid, char player) {
         System.out.print("Enter the coordinates: ");
-        String y_s = scanner.next();
-        String x_s = scanner.next();
+        String yString = scanner.next();
+        String xString = scanner.next();
         int y;
         int x;
-        if (isInt(y_s) && isInt(x_s)) {
-            y = Integer.parseInt(y_s);
-            x = Integer.parseInt(x_s);
+        if (isInt(yString) && isInt(xString)) {
+            y = Integer.parseInt(yString);
+            x = Integer.parseInt(xString);
         } else {
             System.out.println("You should enter numbers!");
-            makePlayerMove(grid);
+            makePlayerMove(grid, player);
             return;
         }
         if (x >= 1 && x <= 3 && y >= 1 && y <= 3) {
-            if (grid[y - 1][x - 1] != '_') {
+            if (grid[y - 1][x - 1] != ' ') {
                 System.out.println("This cell is occupied! Choose another one!");
-                makePlayerMove(grid);
+                makePlayerMove(grid, player);
             } else {
-                grid[y - 1][x - 1] = 'X';
+                grid[y - 1][x - 1] = player;
             }
         } else {
             System.out.println("Coordinates should be from 1 to 3!");
-            makePlayerMove(grid);
+            makePlayerMove(grid, player);
         }
     }
 
@@ -82,74 +79,29 @@ public class Main {
         return true;
     }
 
-    public static void printGameState(char[][] grid) {
-        int oCnt = 0;
-        int xCnt = 0;
-        int cnt = 0;
-        int errorCnt = 0;
-        int xWins;
-        int oWins;
-        for (char[] chars : grid) {
-            for (char aChar : chars) {
-                if (aChar == 'X') {
-                    xCnt++;
-                } else if (aChar == 'O') {
-                    oCnt++;
-                } else if (aChar == '_') {
-                    cnt++;
-                } else {
-                    errorCnt++;
-                }
-            }
-        }
-        if (Math.abs(xCnt - oCnt) > 1) {
-            errorCnt++;
-        }
-        xWins = checkWin(grid, 'X');
-        oWins = checkWin(grid, 'O');
-        if (xWins > 1 || oWins > 1 || (xWins == 1 && oWins == 1)) {
-            errorCnt++;
-        }
-        if (errorCnt > 0) {
-            System.out.println("Impossible");
-        } else if (xWins == 1) {
-            System.out.println("X wins");
-        } else if (oWins == 1) {
-            System.out.println("O wins");
-        } else if (cnt > 0) {
-            System.out.println("Game not finished");
-        } else {
-            System.out.println("Draw");
-        }
-    }
-
-    public static int checkWin(char[][] grid, char player) {
-        int wins = 0;
+    public static boolean checkWin(char[][] grid, char player) {
         if (grid[0][0] == player && grid[0][1] == player && grid[0][2] == player) {
-            wins++;
+            return true;
         }
         if (grid[1][0] == player && grid[1][1] == player && grid[1][2] == player) {
-            wins++;
+            return true;
         }
         if (grid[2][0] == player && grid[2][1] == player && grid[2][2] == player) {
-            wins++;
+            return true;
         }
         if (grid[0][0] == player && grid[1][0] == player && grid[2][0] == player) {
-            wins++;
+            return true;
         }
         if (grid[0][1] == player && grid[1][1] == player && grid[2][1] == player) {
-            wins++;
+            return true;
         }
         if (grid[0][2] == player && grid[1][2] == player && grid[2][2] == player) {
-            wins++;
+            return true;
         }
         if (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player) {
-            wins++;
+            return true;
         }
-        if (grid[2][0] == player && grid[1][1] == player && grid[0][2] == player) {
-            wins++;
-        }
-        return wins;
+        return grid[2][0] == player && grid[1][1] == player && grid[0][2] == player;
     }
 
     public static void printGrid(char[][] grid) {
